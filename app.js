e0f3d0dccf06fcb268a58a385af3c189ba843c8c
DecayP <prvdk.dkp@gmail.com>
2021-01-28 19:22:12 +0530

    demo2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Pranav\'s demo web-app on AWS'));
+app.get('/', (req, res) => res.send('Pranavs demo web-app on AWS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
